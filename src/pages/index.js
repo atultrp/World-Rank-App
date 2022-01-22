f44087a0d7c2d9b32cbbd5229db8008126198ab3
atultrp <atul31815@gmail.com>
2022-01-22 22:46:28 +0530

    Initial Commit

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Layout from '../components/Layout/Layout'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return <Layout>main</Layout>
+}
